618fcfe406f5a5f0770e42d067f4afa0432c16f2
Roberto Nicolau <roberto.leonardo.2000@gmail.com>
2021-02-02 21:53:57 -0300

    Adição dos componentes de exibição  da transação

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -16,13 +17,13 @@ class MyHomePage extends StatelessWidget {
   final _transactions = [
     Transaction(
       id: 't1',
-      title: 'Tenis',
+      title: 'Tenis da Nike',
       value: 310.79,
       date: DateTime.now(),
     ),
     Transaction(
       id: 't2',
-      title: 'Curso',
+      title: 'Curso Flutter',
       value: 22.99,
       date: DateTime.now(),
     ),
@@ -41,12 +42,62 @@ class MyHomePage extends StatelessWidget {
           Container(
             height: 100,
             child: Card(
-              child: Text('Gráfico'),
+              color: Colors.lightBlue[100],
+              child: Text(
+                'Gráfico',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
               elevation: 5,
             ),
           ),
-          Card(
-            child: Text('Lista de transações'),
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        'R\$ ${tr.value.toString()}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tr.title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Text(
+                          DateFormat('dd MMM y').format(tr.date),
+                          style: TextStyle(color: Colors.blueGrey[600]),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
           )
         ],
       ),
